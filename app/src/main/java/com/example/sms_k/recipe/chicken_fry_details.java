395911b3d2f906f6e4e2c5d96cef56e421cb2344
Meta Coders <metacoders18@gmail.com>
2018-09-07 01:03:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sms_k/recipe/chicken_fry_details.java
@@ -0,0 +1,13 @@
+package com.example.sms_k.recipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chicken_fry_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chicken_fry_details);
+    }
+}
